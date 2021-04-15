37f9716217c809358184d935af4cead168391eed
Alex Hartmann <23240822+alhman@users.noreply.github.com>
2021-04-15 21:55:11 +0200

    Add basic rich text support (WIP)

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -56,7 +56,9 @@ module.exports = {
         { from: 'node_modules/autosize/dist/autosize.js', to: 'ext/autosize.js' },
         { from: 'node_modules/jsbarcode/dist/JsBarcode.all.min.js', to: 'ext/JsBarcode.all.min.js' },
         { from: 'node_modules/spectrum-colorpicker/spectrum.css', to: 'ext/spectrum.css'},
-        { from: 'node_modules/spectrum-colorpicker/spectrum.js', to: 'ext/spectrum.js' }
+        { from: 'node_modules/spectrum-colorpicker/spectrum.js', to: 'ext/spectrum.js' },
+        { from: 'node_modules/quill/dist/quill.snow.css', to: 'ext/quill.snow.css'},
+        { from: 'node_modules/quill/dist/quill.js', to: 'ext/quill.js' }
       ]
     }),
   ]
