b53c220b892f41d431487ab4ad7ff3c7ef92de21
Alex Hartmann <23240822+alhman@users.noreply.github.com>
2021-04-27 14:46:41 +0200

    Add missing method for empty panel

--- src/panels/EmptyDetailPanel.js
+++ src/panels/EmptyDetailPanel.js
@@ -26,6 +26,10 @@ export default class EmptyDetailPanel {
         $('#rbro_empty_detail_panel').addClass('rbroHidden');
     }
 
+    isKeyEventDisabled() {
+        return false;
+    }
+
     notifyEvent(obj, operation) {
     }
 
